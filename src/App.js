89588d96b69697e9e4705e7b7542565fb9b9cefd
nandini2399 <nandinijairath@gmail.com>
2025-08-19 21:55:13 +0530

    Structure setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-green-800 text-2xl">
-      Lets build netflix gpt
-    </div>
+    <Body />
   );
 }
 
