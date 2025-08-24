ad51aa943692161034fe6f0b1dc0184062c29d8a
nandini2399 <nandinijairath@gmail.com>
2025-08-24 18:56:26 +0530

    Login Page UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import logo from "../utils/logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='absolute px-2 py-1 bg-gradient-to-b from-red-300 z-10'>
+      <img className='w-40' src={logo} />
+    </div>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import Header from './Header';
+import background from '../utils/background.jpg'
+import { useState } from 'react';
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const handleClick = ()=>{
+    setIsSignIn(!isSignIn)
+  }
   return (
-    <div>Login</div>
+    <div>
+      <Header />
+      <div className='absolute'>
+        <img src={background} alt='Background'/>
+      </div>
+      <div className="flex items-center justify-center h-full relative z-10">
+        <form className="w-3/12 p-12 bg-black bg-opacity-60 rounded-lg shadow-lg mt-40">
+          <h1 className='font-bold text-white text-3xl py-4'>
+          {isSignIn?"Sign In":"Sign Up"}  
+          </h1>
+          {!isSignIn && <input type="text" placeholder="Full Name" className="p-2 my-4 w-full bg-gray-900 rounded" />}
+          <input type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-gray-900 rounded" />
+          <input type="password" placeholder="Password" className="p-2 my-4 w-full bg-gray-900 rounded" />
+          <button className="p-3 my-4 w-full bg-red-600 text-white rounded">{isSignIn?"Sign In":"Sign Up"}</button>
+          <p onClick={handleClick}
+          className='text-white cursor-pointer hover:text-blue-500'>
+          {isSignIn?"New to Stream-gpt? Sign up Now!":"Already registered? Sign in Now!"}  
+          </p>
+        </form>
+      </div>
+    </div>
+    
   )
 }
 
